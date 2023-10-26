a6d12b924dee2590395a008e021883db719d8746
ReneSRM <124410445+ReneSRM@users.noreply.github.com>
2023-10-25 19:32:02 -0700

    25/10/23

--- parcial2/home.php
+++ parcial2/home.php
@@ -20,20 +20,19 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
             </li>
             <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Menú
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Consultar alumnos</a></li>
-                <li><a class="dropdown-item" href="#">Registrar alumnos</a></li>
+                <li><a class="dropdown-item" href="consultarAlumnos.php">Consultar alumnos</a></li>
+                <li><a class="dropdown-item" href="RegistrarAlumnos.php">Registrar alumnos</a></li>
                 <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Menú</a></li>
+                <li><a class="dropdown-item" href="menu.php">Menú</a></li>
             </ul>
             </li>
             
